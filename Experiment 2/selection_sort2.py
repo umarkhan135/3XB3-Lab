8f033e200733fd6157f31eb34bc16c16e65cd6ae
Omar El Aref <o.k.elaref@gmail.com>
2023-09-21 12:58:04 -0400

    selecetion sort done

--- Experiment 2/selection_sort2.py
+++ Experiment 2/selection_sort2.py
@@ -2,22 +2,20 @@ def swap(L, i, j):
     L[i], L[j] = L[j], L[i]
 
 
-def swap(L, i, j):
-    L[i], L[j] = L[j], L[i]
-
 
 def selection_sort(L):
     for i in range(0, len(L)//2):
         min_index, max_index = find_min_index(L, i)
-        swap(L, i, min_index)
+        if min_index != len(L) - i:
+            swap(L, i, min_index)
         swap(L, len(L) - i - 1, max_index)
-        print(max_index)
-        print(L)
+        
+        
 
 
 def find_min_index(L, n):
     min_index = n
-    max_index = n
+    max_index = len(L) - n - 1
     for i in range(n+1, len(L) - n):
         if L[i] < L[min_index]:
             min_index = i
@@ -27,7 +25,7 @@ def find_min_index(L, n):
 
 
 list = [7, 2, 5, 9, 1, 4, 3, 6, 8]
-list2 = [2,4,3,1]
-selection_sort(list)
-print(list)
+list2 = [9,8,7,6,5,4,3,2,1]
+selection_sort(list2)
+print(list2)
 
