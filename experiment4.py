f4e30c9edfe1844db89a7b6210fa2adf4c7d9bb6
Timothy Pokanai <pokanait@mcmaster.ca>
2023-10-02 10:20:30 -0400

    Experiment 3 and 4 graphs included

--- experiment4.py
+++ experiment4.py
@@ -1,57 +1,52 @@
-from good_sorts import *
+from good_sorts import quicksort
+from good_sorts import mergesort
+from good_sorts import heapsort
 from bad_sorts import create_random_list
+
+import matplotlib.pyplot as plt
 import timeit
 
 max_value = 1000000
+list_lengths = list(range(0, 31000, 1000))
 
-def average(L):
-    return sum(L)/len(L)
-
-def quicksort_test():
-    average_quicksort_times = []
-    for i in range(0, 100000, 10000):
-        time = 0
-        for _ in range(10):
-            L = create_random_list(i, max_value)
-            start = timeit.default_timer()
-            quicksort(L)
-            end = timeit.default_timer()
-            time += end - start
-        average_quicksort_times.append(time/10)
+def sorting_test(sorting_algorithm, L):
+    time = 0
+    start = timeit.default_timer()
+    sorting_algorithm(L)
+    end = timeit.default_timer()
+    time += end - start
     
-    return average_quicksort_times
-
-def mergesort_test():
-    average_mergesort_times = []
-    for i in range(0, 100000, 10000):
-        time = 0
-        for _ in range(10):
-            L = create_random_list(i, max_value)
-            start = timeit.default_timer()
-            mergesort(L)
-            end = timeit.default_timer()
-            time += end - start
-        average_mergesort_times.append(time/10)
-
-    return average_mergesort_times
-
-def heapsort_test():
-    average_heapsort_times = []
-    for i in range(0, 100000, 10000):
-        time = 0
-        for _ in range(10):
-            L = create_random_list(i, max_value)
-            start = timeit.default_timer()
-            heapsort(L)
-            end = timeit.default_timer()
-            time += end - start
-        average_heapsort_times.append(time/10)
-
-    return average_heapsort_times
-
+    return time
+    
 def experiment4():
-    print(quicksort_test())
-    print(mergesort_test())
-    print(heapsort_test())
+    avg_quicksort_times = []
+    avg_mergesort_times = []
+    avg_heapsort_times = []
+    for i in list_lengths:
+        quicksort_time = 0
+        mergesort_time = 0
+        heapsort_time = 0
+        for _ in range(10):
+            quicksort_list = create_random_list(i, max_value)
+            mergesort_list = quicksort_list.copy()
+            heapsort_list = quicksort_list.copy()
+
+            quicksort_time += sorting_test(quicksort, quicksort_list)
+            mergesort_time += sorting_test(mergesort, mergesort_list)
+            heapsort_time += sorting_test(heapsort, heapsort_list)
+        
+        avg_quicksort_times.append(quicksort_time/10)
+        avg_mergesort_times.append(mergesort_time/10)
+        avg_heapsort_times.append(heapsort_time/10)
+
+    plt.plot(list_lengths, avg_quicksort_times, label='Quick Sort')
+    plt.plot(list_lengths, avg_mergesort_times, label='Merge Sort')
+    plt.plot(list_lengths, avg_heapsort_times, label='Heap Sort')
+
+    plt.xlabel('List Length')
+    plt.ylabel('Average Execution Time (s)')
+    plt.title('Quicksort VS Mergesort VS Heapsort')
+    plt.legend()
+    plt.show()
 
 experiment4()
